7a24ee5f12a78c6f065a41a989d51406fa5e95e8
Sintexte <sintexte@gmail.com>
2020-06-15 18:17:49 +0200

    Successfully Rendered an Html Page

--- Log_Off/Log_Off/settings.py
+++ Log_Off/Log_Off/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Log_Off.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
