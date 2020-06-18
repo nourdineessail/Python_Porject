f243be23484a38c6a9c4c37b127fce7c848a7e90
Sintexte <sintexte@gmail.com>
2020-06-18 23:25:27 +0200

    script placement changed

--- Log_Off/static/js/home.js
+++ Log_Off/static/js/home.js
@@ -25,19 +25,14 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
               }
       }
   
-  });
-  
-  $('.tab a').on('click', function (e) {
-    
+});
+
+$('.tab a').on('click', function (e) {
     e.preventDefault();
-    
     $(this).parent().addClass('active');
     $(this).parent().siblings().removeClass('active');
-    
     target = $(this).attr('href');
-  
     $('.tab-content > div').not(target).hide();
-    
     $(target).fadeIn(600);
     
   });
